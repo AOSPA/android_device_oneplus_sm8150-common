0a9f7e6e33fc2d7d5f00dee013bd346deae4e1f6
LuK1337 <priv.luk@gmail.com>
2018-07-16 13:22:31 +0200

    sm8150-common: Add ANT+ support
    Change-Id: I3c82e0300b0c88c4eccea987b8c8942f8a35231d

--- sm8150.mk
+++ sm8150.mk
@@ -63,6 +63,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0 \
