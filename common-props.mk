6bffd6eb4b1f533b13509a6bd8ddc005b1edbdcf
Midas Chien <midaschieh@google.com>
2019-05-03 17:52:04 +0800

    sm8150: Enable high fps sf early phase offsets
    Change sf early phase offset for 90hz to be early=6.1ms,
    early_gl=9ms to give the same ~16ms and ~13ms they had on 60hz.
    
    Bug: 130191039
    Test: launch app and observe systraces, frame drop 1.83% -> 0.37%
    Test: scrolling on photo with force GL compos and observe systraces,
          frame drop 1.61% -> 0.84%
    
    [PA Note: since we enabled Variable Refresh Rate, this commit uses 60Hz
    offsets on 60Hz and 90Hz offsets on 90Hz.  Before, since we only supported
    90Hz, we used 90Hz offsets all the time.]
    
    Change-Id: Icbd734d9fa217333741bd625e715eae973a3e368

--- common-props.mk
+++ common-props.mk
@@ -58,14 +58,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.qemu.gles=0 \
     persist.vendor.color.matrix=2 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    debug.sf.early_phase_offset_ns=6100000 \
-    debug.sf.early_gl_phase_offset_ns=9000000 \
+    debug.sf.early_phase_offset_ns=500000 \
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
     debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000 \
     debug.sf.enable_gl_backpressure=1
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=1000000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.vsync_event_phase_offset_ns=2000000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
