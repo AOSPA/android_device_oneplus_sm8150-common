8087c582a962e3b938eaeb295198ea6f4529abba
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2019-10-21 14:10:39 +0000

    sm8150-common: more changes.
    Rebase properly later.
    
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,6 @@ TARGET_USES_UEFI := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -152,9 +151,6 @@ BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp /vendor/firmware_mnt:/firmware /ve
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
-
-# SELinux
-PRIVATE_EXCLUDE_BUILD_TEST := true
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
