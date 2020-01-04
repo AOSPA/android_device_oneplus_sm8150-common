2b04fd65b7a11f1e7d75d88f31478ce36e521be3
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-04 17:48:11 +0900

    sm8150-common: enable automatic refresh rate switching
    Change-Id: I67d727cb1630c5ea6a3d5db2d9abdbe6e8453079
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- common-props.mk
+++ common-props.mk
@@ -51,6 +51,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sensor.not_support_rbg=1 \
     ro.vendor.sensor.not_support_rbg=1
 
+# Automatic refresh rate switching for power savings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_smart_90_for_video=true
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
