56bc8615bb7b5f9481a631a4b8d5fc1152f4735a
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-08 21:27:51 +0100

    sm8150-common: Disable automatic refresh rate switching
      * On OP7T Pro it causes strange gamma behaviour so lets disable it for now
    
    Change-Id: I9d44c74c5fbdf0ad30da00130b024b2a26e0532e

--- common-props.mk
+++ common-props.mk
@@ -51,12 +51,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sensor.not_support_rbg=1 \
     ro.vendor.sensor.not_support_rbg=1
 
+ifneq ($(TARGET_IS_ONEPLUS_T_DEVICE),true)
 # Automatic refresh rate switching for power savings
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=500 \
     ro.surface_flinger.set_touch_timer_ms=750 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_smart_90_for_video=true
+endif
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
