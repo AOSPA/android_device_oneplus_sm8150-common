780ca5003d02a11338c370d639806ca40da1d5e7
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-08 23:26:52 +0100

    sm8150-common: Make automatic refresh rate switching less aggressive
     * While switching between 60/90 Hz it causes strange gamma behavior so lets make the switching less aggressive
    
    Change-Id: I242f80f1f2035e4b6e41f05e192e6d63d16cb067

--- common-props.mk
+++ common-props.mk
@@ -53,9 +53,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Automatic refresh rate switching for power savings
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=750 \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=2000
+    ro.surface_flinger.set_touch_timer_ms=1000
+    ro.surface_flinger.set_display_power_timer_ms=2000
     ro.surface_flinger.use_smart_90_for_video=true
 
 # Graphics
