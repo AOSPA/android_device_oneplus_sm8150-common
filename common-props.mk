38d39eb9a1de21676bd2651ed43f456d589b24ff
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-16 02:45:58 +0900

    sm8150-common: relax timers for automatic refresh rate switching
    Due to visible gamma shifts on our panels, we should make the switchings
    to happen conservatively.
    
    With idle timer >= touch timer, idle timer now always takes precedence,
    but we're leaving touch timer property to immediately switch back to
    90 Hz on touch input events.
    
    Display power timer is left untouched as it's responsible for display
    mode changes such as Doze.
    
    Change-Id: If8b1e00a2d2213c4d2288ab28fad4ba0826a36e8
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- common-props.mk
+++ common-props.mk
@@ -53,8 +53,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Automatic refresh rate switching for power savings
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_smart_90_for_video=true
 
