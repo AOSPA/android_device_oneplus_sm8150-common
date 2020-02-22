969a495d2eb7b7ba3b904aa5680cc4864b380d21
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-02-22 18:56:00 +0200

    sm8150-common: fod: Use new FOD dimming alpha node
    Change-Id: Ifb5e5a7caf506b86bc5503a7eb8c452e6f982b2b

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -35,7 +35,7 @@
 
 // This is not a typo by me. It's by OnePlus.
 #define HBM_ENABLE_PATH "/sys/class/drm/card0-DSI-1/op_friginer_print_hbm"
-#define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/dim_alpha"
+#define DIM_AMOUNT_PATH "/sys/class/drm/card0-DSI-1/fod_dim_alpha"
 
 namespace vendor {
 namespace pa {
