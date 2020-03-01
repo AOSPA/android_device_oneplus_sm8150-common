6130b508ebeacaf346ebf30611aaef172d5c7e09
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-02 05:32:15 +0900

    sm8150-common: post_boot: setup 4GB of vnswap
    Change-Id: Ica2de2f2b0c486b4c1fee9852af49466aad7e89e
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1,5 +1,12 @@
 #! /vendor/bin/sh
 
+# Setup 4GB of vnswap
+echo 4294967296 > /sys/devices/virtual/block/vnswap0/disksize
+echo 130 > /proc/sys/vm/swappiness
+# System mkswap behaves incorrectly with vnswap
+mkswap /dev/block/vnswap0
+swapon /dev/block/vnswap0
+
 # Enable SIGKILL memory reap
 echo 1 > /proc/sys/vm/reap_mem_on_sigkill
 
