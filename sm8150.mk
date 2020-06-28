d9b1807160960fbf11178b133f4431b9f6612c29
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2020-06-28 23:30:07 +0000

    sm8150-common: move qti and hotword permissions to vendor/pa.
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Change-Id: I610025033da3a1651ea7f1eff5cc3da02df4b355

--- sm8150.mk
+++ sm8150.mk
@@ -107,9 +107,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstabDyn.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 endif
 
-# Hotword Enrollement
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
 # Input
 PRODUCT_COPY_FILES += \
@@ -157,10 +154,6 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     power.qcom
-
-# QCOM
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # Sensors
 PRODUCT_PACKAGES += \
