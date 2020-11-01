218c0a30c81dcacccdcf478475f9402ea4c8e09a
Łukasz Patron <priv.luk@gmail.com>
2020-11-01 01:44:13 -0400

    sm8150-common: Include VNDK 29 prebuilts
    Change-Id: If40a3e4133c2227dfdfdb45bb5105491e358f7d1

--- sm8150.mk
+++ sm8150.mk
@@ -32,6 +32,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 29
+PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_TARGET_VNDK_VERSION)
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
