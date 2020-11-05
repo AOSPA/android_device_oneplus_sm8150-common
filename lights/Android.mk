c0f8405490cdc8c1bc542eb03c5405cb8a7a1f2f
Chris Crump <chris@cypheros.co>
2020-11-05 12:59:19 -0500

    sm8150-common: lights: Move to .bp
    Change-Id: Ib929bc15cf7fcbfe484d88ee133f00a9b99dfeac

--- lights/Android.mk
+++ lights/Android.mk
@@ -2,34 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_msmnile
-LOCAL_MODULE_TAGS  := optional
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service
-
-LOCAL_SRC_FILES := \
-    service.cpp \
-    Light.cpp
-
-LOCAL_REQUIRED_MODULES := \
-    android.hardware.light@2.0-service.oneplus_msmnile.rc
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    libhidlbase \
-    liblog \
-    android.hardware.light@2.0
-
-LOCAL_STATIC_LIBRARIES := \
-    libbase \
-    libutils
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
 LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_msmnile.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
