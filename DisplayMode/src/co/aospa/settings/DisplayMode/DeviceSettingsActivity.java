7739fd8c1dc7108b070298129aeb2f88d982557b
Vishalcj17 <vishalcj@aospa.co>
2022-12-17 19:03:44 +0900

    DisplayMode: settingslib.collapsingtoolbar.R -> settingslib.widget.R
     * Fixes "error: unresolved reference: R"
    
    Change-Id: Ib49a86efb4cdecdf21a818fe6d3886b4a191bb12

--- DisplayMode/src/co/aospa/settings/DisplayMode/DeviceSettingsActivity.java
+++ DisplayMode/src/co/aospa/settings/DisplayMode/DeviceSettingsActivity.java
@@ -20,7 +20,7 @@ package co.aospa.settings.DisplayMode;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
 
