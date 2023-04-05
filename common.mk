d2888fbce23cd18ab83fb2cac0e0575bfa1f49da
TimoDo <timo.capa@gmail.com>
2023-04-05 20:35:17 +0200

    common: ro.charger.enable_suspend was moved to vendor/aospa
    Change-Id: I8884a0dba8c85056f1ff39dbef3add3cc7b27571

--- common.mk
+++ common.mk
@@ -106,10 +106,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# Charger
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.charger.enable_suspend=true
-
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
