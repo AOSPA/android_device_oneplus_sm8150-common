3b2cc4f5c2a0104f7a68840e1ff84935b4a6b85c
Jake Weinstein <jake@aospa.co>
2023-07-02 14:35:22 +0000

    Revert "sm8150-common: Disable getVsyncPeriodFromHWC"
    This reverts commit 57a7a2d456c51778d6d8378ba99de478aef92358.
    
    Reason for revert: moved to vendor/aospa
    
    Change-Id: I453cae6f9f015e0e84365389a20f66dc7ed97dc5

--- common.mk
+++ common.mk
@@ -123,9 +123,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=500 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sf.use_latest_hwc_vsync_period=0
-
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.display.ad.hdr_calib_data=/vendor/etc/hdr_config.cfg \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
