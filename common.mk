87cde29ddc2b336b3c7bea89ba229bd9469a7fb9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    sm8150-common: Introduce DolbyManager
    - Converted Xiaomi dirac implementation based on observing
      Xiaomi sound effects app and daxservice.
    
    - Thanks to jhenrique09 for the hack from Xiaomi dirac parts
      "Pause/play music stream to get effects applied".
    
    Change-Id: I2de96ba526284dbbb7eb6c85d1963cf5937e8664
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common.mk
+++ common.mk
@@ -53,6 +53,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+PRODUCT_PACKAGES += \
+    DolbyManager
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
